4321b4cfa9885f2771b3954744c3b2ab67602dbc
Janko <mailodjanka@gmail.com>
2020-02-08 15:28:38 +0100

    Created a class Car and a paint method. Setup the rspec and tested that the paint method works

---
+++ car.rb
@@ -0,0 +1,16 @@
+class Car
+
+    attr_accessor :color
+
+    def initialize
+        @color = 'red'
+    end
+
+
+    def paint(new_color)
+        @color = new_color
+    end
+end
+
+
+  
---
+++ spec/car_spec.rb
@@ -0,0 +1,10 @@
+require './car.rb'
+
+describe Car do
+
+    it 'Color of the car is changed' do
+    subject.paint('green')
+    expect(subject.color).to eq 'green'
+    end
+
+end
